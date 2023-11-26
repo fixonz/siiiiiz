e6aaa598937389053b5d81d5e45a237e42844986
fixonz <109214021+fixonz@users.noreply.github.com>
2023-11-26 21:03:25 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,8 +2,10 @@ import fs from "fs";
 import path from "node:path";
 import { Client, Events, GatewayIntentBits, Collection } from "discord.js";
 import dotenv from "dotenv";
+import getBuyCost from "./endpoints/SalePriceHistory.js";
 
 dotenv.config();
+
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -34,13 +36,15 @@ client.on(Events.InteractionCreate, async (interaction) => {
       ephemeral: true,
     });
   }
-  console.log(interaction);
 });
 
 // When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
+client.once(Events.ClientReady, async (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
+  
+  // Call the getBuyCost function here or wherever you need to use it
+  const buyCost = await getBuyCost();
+  console.log(JSON.stringify(buyCost));
 });
 
 // Log in to Discord with your client's token
